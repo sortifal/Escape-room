c40ccb99db38f3adb17ba994b7b753f0eebf6121
alexis Bruneteau <alexis.bruneteau@epita.fr>
2019-12-30 16:04:42 +0100

    Menu Improvments

---
+++ Escape Room/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject main;
+    public GameObject options;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        main.SetActive(true);
+        options.SetActive(false);
+    }
+   public void HideMenu()
+    {
+        main.SetActive(false);
+        options.SetActive(true);
+    }
+    public void ShowMenu()
+    {
+        main.SetActive(true);
+        options.SetActive(false);
+    }
+}
